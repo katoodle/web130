fac4ba2f6f118dfc46fd50a255e7717e8ccbab32
kt <katoodle@gmail.com>
2014-08-23 23:36:18 +0000

    form validation

--- classwork/07-form-validation/form_handler.php
+++ classwork/07-form-validation/form_handler.php
@@ -1,10 +1,6 @@
 <?php
 
-/*
-•• server-side file receives information from the submitted form
-** user authentication and authorization handled here
-*/  
 
-echo "<h1>Success! Form passed client-side validation.";
+echo "<h1>Yay! You should now feel validated!</h1>";
 
 ?>
